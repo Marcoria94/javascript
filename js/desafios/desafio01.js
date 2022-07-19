213662ad76c2cfec7cea5c82e9e65ea9e92653d3
Mariano Coria <marcoria1994@gmail.com>
2022-07-18 21:13:59 -0300

    modificando desafio

--- js/desafios/desafio01.js
+++ js/desafios/desafio01.js
@@ -1,43 +1,45 @@
-console.log ("Desafio")
+console.log("Desafio")
 
-function descuento (numero, porcentaje) {
+function descuento(numero, porcentaje) {
     let totalDesc = numero * porcentaje / 100
     let precioFinal = numero - totalDesc
     return precioFinal
 }
 
 
-function impuesto (numero, porcentaje) {
+function impuesto(numero, porcentaje) {
     let impuesto = numero * porcentaje / 100
     let totalImp = impuesto + numero
     return totalImp
-} 
-
-let opcionUsuario = parseInt(prompt("Seleccione 1 para descuento , seleccione 2 para impuesto"));
-
-if (opcionUsuario === 1 ) {
-    let numero = parseInt (prompt ("Ingresa un numero"));
-    let porcentaje = parseInt (prompt ("Ingresa el % de descuento"));
-    console.log("opcion1")
-    alert("El precio final es: $" + descuento(numero,porcentaje));
-    
-}else if (opcionUsuario === 2) {
-    let numero = parseInt (prompt ("Ingresa un numero"));
-    let porcentaje = parseInt (prompt ("Ingresa el % de impuesto o descuento"));
-    console.log("opcion2")
-    alert("Total con impuesto es: $" + impuesto (numero,porcentaje));
-}else{
-    alert("Ingrese una de las opciones")
 }
+let numero = parseInt (prompt ("Ingresa un numero"));
+let porcentaje = parseInt (prompt ("Ingresa el % de descuento o interes"));
+let opcionUsuario = parseInt(prompt("Seleccione 1 para descuento , seleccione 2 para impuesto"));
+let pregunta = 1 // Se agrega para darle el valor de entero al ciclo
+do {
+    switch (opcionUsuario) {
+        case 1:
+            
+            alert("El precio final es: $" + descuento(numero, porcentaje));
+
+            break;
+        case 2:
+            alert("El precio final es: $" + impuesto(numero, porcentaje));
+
+        
+
+            break;
+
+        default: alert("El dato ingresa es invalido") 
+            break;
+    }
+    pregunta = prompt('¿Desea hacer alguna otra accion? \n1.-Si \t2.-No') //se pregunta por el nuevo valor 
+    if (pregunta == 1){
+        numero = parseInt (prompt ("Ingresa un numero"));
+        porcentaje = parseInt (prompt ("Ingresa el % de descuento o interes"));
+        opcionUsuario = parseInt(prompt("Seleccione 1 para descuento , seleccione 2 para impuesto"));
+    }
 
 
-
-
-
-
-
-
-
-
-    
+} while (pregunta == 1);
 
